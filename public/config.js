d427f405bed8f505929a3e5ab231d50587391c08
samdramaya251-sys <samdramaya251@gmail.com>
2026-06-22 12:52:53 -0600

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,1 +1,9 @@
-let _CONFIG = {};
+let _CONFIG = {  
+    wisp: "wss://wisp-server.replit.app/wisp/",
+    engines: {
+        google: "https://www.google.com/search?q=%s",
+        duckduckgo: "https://duckduckgo.com/?q=%s",
+        bing: "https://www.bing.com/search?q=%s",
+        yahoo: "https://search.yahoo.com/search?p=%s"
+    }
+};
